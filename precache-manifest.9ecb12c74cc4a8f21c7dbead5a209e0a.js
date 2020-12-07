1c66906d1f28d5ee3c02afc452e963db8fb34381
jatin77 <clockstopfountain@gmail.com>
2020-12-07 19:45:12 +0530

    Updates

---
+++ precache-manifest.9ecb12c74cc4a8f21c7dbead5a209e0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628f60a0cbb6fb3014d91fb5df9b74af",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "a77f66901716712a7538",
+    "url": "/invoice/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b8967ee14c0861ea4008",
+    "url": "/invoice/static/css/main.0e662985.chunk.css"
+  },
+  {
+    "revision": "a77f66901716712a7538",
+    "url": "/invoice/static/js/2.cc9296fc.chunk.js"
+  },
+  {
+    "revision": "2aaf942c12d1fcc3acf1d06dd11804f5",
+    "url": "/invoice/static/js/2.cc9296fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8967ee14c0861ea4008",
+    "url": "/invoice/static/js/main.3ec33575.chunk.js"
+  },
+  {
+    "revision": "76d00b1377d3a1a2e48a",
+    "url": "/invoice/static/js/runtime-main.7e686f64.js"
+  }
+]);
